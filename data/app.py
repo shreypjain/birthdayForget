0c76496c9071ab4335e07cca2b7dd49a1d0c7194
shreypjain <jainshreyp@gmail.com>
2020-11-04 17:56:06 -0500

    Added the new date time stamps

--- data/app.py
+++ data/app.py
@@ -1,17 +1,24 @@
 import pymongo
 import pandas
+from datetime import datetime
 
 df = pandas.read_csv('birthdays.csv')
 birthdays = []
 for i in df.itertuples():
-    month = i[3][0:2]
-    day = i[3][3:5]
-    year = i[3][6:8]
-    print(f'20{year}')
+    month = int(i[3][0:2])
+    day = int(i[3][3:5])
+    year = int(i[3][6:8])
+    if year > 21:
+        year+=19*100
+    else:
+        year+=20*100
+    bday = datetime.timestamp(datetime(year,month,day)) * 1000
+    print(f'birthday: {50 - (bday / 31536000000)}')
     birthdays.append({
         'firstName':i[0],
         'lastName':i[1],
         'birthday':i[2],
         'age':i[3]
     })
-# print(birthdays)
+#print(datetime.timestamp(datetime(2020,11, 4)) * 1000)
+
